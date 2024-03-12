98ee7847ed77a9829cb21ced282c190a6b713ef9
bhuvanrandhir <141033190+bhuvanrandhir@users.noreply.github.com>
2024-03-13 00:55:47 +0530

    Practical_02_a.c

--- Practical_02_a.c
+++ Practical_02_a.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    printf("Current System Time : %s\n",__TIME__);
-    printf("Current Date According to the system is: %s\n",__DATE__);
-    printf("Current line where you work now:%d\n",__LINE__);
+    printf("Current System Time is: %s\n",__TIME__);
+    printf("Current Date of the system is: %s\n",__DATE__);
+    printf("Current line is :%d\n",__LINE__);
     printf("Current File Name: %s\n",__FILE__);
     return 0;
 
