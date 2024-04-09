c9b65563c9482fcbf0221b230093b53ab4132056
bhuvanrandhir <141033190+bhuvanrandhir@users.noreply.github.com>
2024-04-09 08:27:25 +0530

    Add files via upload
    Bubble and Selection Sort

---
+++ Practical_05_a.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for (int j = i+1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++)
+        for (int j = 0; j < n-i-1; j++)
+            if (arr[j] > arr[j+1])
+                swap(&arr[j], &arr[j+1]);
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Original array: \n");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+    printf("Array sorted using Selection Sort: \n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+    printf("Array sorted using Bubble Sort: \n");
+    printArray(arr, n);
+
+    return 0;
+}
